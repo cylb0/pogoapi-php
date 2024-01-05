487cfb8d0cd0136ffb555a42af2c53adbda4dd5c
Morgan <morgan.foucaut@greta-formations.fr>
2024-01-05 04:07:22 +0100

    updated execute_migrations file

--- Scripts/execute_migrations.php
+++ Scripts/execute_migrations.php
@@ -2,13 +2,30 @@
 
 require_once(__DIR__ . '/../Config/Database.php');
 require_once(__DIR__ . '/../Migrations/CreateUserTable.php');
+require_once(__DIR__ . '/../Fixtures/Fixtures.php');
+require_once(__DIR__ . '/../Models/User.php');
 
-$database = Database::getInstance();
-$migrations = [
-    new CreateUserTable($database)
-];
-
-foreach ($migrations as $migration) {
-    echo($migration->down() . '</br>');
-    echo($migration->up() . '</br>');
-};
+try {
+    $database = Database::getInstance();
+    $migrations = [
+        new CreateUserTable($database)
+    ];
+    
+    foreach ($migrations as $migration) {
+        echo($migration->down() . '</br>');
+        echo($migration->up() . '</br>');
+    };
+    
+    $fixtures = new Fixtures();
+    
+    // Insertion des fixtures users
+    foreach ($fixtures->usersFixtures() as $fixture) {
+        $database->getPdo()->exec("INSERT INTO users (username, password, email) VALUES ('{$fixture['username']}', '{$fixture['password']}', '{$fixture['email']}')");
+        $id = $database->getPdo()->lastInsertId();
+        $user = new User($id, $fixture['username'], $fixture['password'], $fixture['email']);
+        $username = $user->getUsername();
+        echo ("Record for user $username has been created." . "</br>");
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
