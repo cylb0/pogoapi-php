b0703edfd95130de154fe65d5e73a64d05f82e1b
Morgan <morgan.foucaut@greta-formations.fr>
2024-01-08 15:43:36 +0100

    Added PDOException error handling to CreateUserTable.php

--- Migrations/CreateUserTable.php
+++ Migrations/CreateUserTable.php
@@ -9,31 +9,38 @@ final class CreateUserTable {
     }
 
     public function up() {
-
-        $check_query = "SHOW TABLES LIKE 'users'";
-        $results = $this->pdo->query($check_query);
-        if ($results->rowCount() > 0) {
-            return('Users table already exists.');
+        try {
+            $check_query = "SHOW TABLES LIKE 'users'";
+            $results = $this->pdo->query($check_query);
+            if ($results->rowCount() > 0) {
+                return('Users table already exists.');
+            }
+    
+            $query = "CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(32) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )";
+            $this->pdo->exec($query);
+    
+            return('Table Users has been created.');
+        } catch (PDOException $e) {
+            return 'Database error while creating Types table: ' . $e->getMessage();
         }
-
-        $query = "CREATE TABLE users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(32) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL UNIQUE,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        )";
-        $this->pdo->exec($query);
-
-        return('Table Users has been created.');
     }
 
     public function down() {
-        $query = "DROP TABLE IF EXISTS users";
-        $this->pdo->exec($query);
-
-        return('Table Users has been deleted.');
+        try {
+            $query = "DROP TABLE IF EXISTS users";
+            $this->pdo->exec($query);
+    
+            return('Table Users has been deleted.');
+        } catch (PDOException $e) {
+            return 'Database error while dropping Types table: ' . $e->getMessage();
+        }
     }
 
 }
