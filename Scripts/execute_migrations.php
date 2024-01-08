1ae79b0b6bb49d043f3ed605c1eccda48d688bf7
Morgan <morgan.foucaut@greta-formations.fr>
2024-01-08 15:53:09 +0100

    New migration for types fixtures

--- Scripts/execute_migrations.php
+++ Scripts/execute_migrations.php
@@ -2,13 +2,16 @@
 
 require_once(__DIR__ . '/../Config/Database.php');
 require_once(__DIR__ . '/../Migrations/CreateUserTable.php');
+require_once(__DIR__ . '/../Migrations/CreateTypeTable.php');
 require_once(__DIR__ . '/../Fixtures/Fixtures.php');
 require_once(__DIR__ . '/../Models/User.php');
+require_once(__DIR__ . '/../Models/Type.php');
 
 try {
     $database = Database::getInstance();
     $migrations = [
-        new CreateUserTable($database)
+        new CreateUserTable($database),
+        new CreateTypeTable($database)
     ];
     
     foreach ($migrations as $migration) {
@@ -25,6 +28,15 @@ try {
         $user = new User($id, $fixture['username'], $fixture['password'], $fixture['email']);
         $username = $user->getUsername();
         echo ("Record for user $username has been created." . "</br>");
+    }
+
+    //Insertion des fixtures type
+    foreach($fixtures->typesFixtures() as $fixture) {
+        $database->getPdo()->exec("INSERT INTO types (name_en) VALUES ('{$fixture['name_en']}')");
+        $id = $database->getPdo()->lastInsertId();
+        $type = new Type($id, $fixture['name_en']);
+        $name_en = $type->getNameEn();
+        echo ("Record for type $name_en has been created.<br>");
     }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
