6ec9c9d4fed2677808a35711c8aaf48ec50b4bf8
Morgan <morgan.foucaut@greta-formations.fr>
2024-01-08 20:02:10 +0100

    TypeRepository, addType(), getTypeById() + test

--- Repositories/TypeRepository.php
+++ Repositories/TypeRepository.php
@@ -12,16 +12,43 @@ class TypeRepository {
     }
 
     //Insert Type in Database
-    public function addType($name_en): Type {
+    public function addType($name_en, $name_fr = ''): Type {
         try {
-            $statement = $this->pdo->prepare('INSERT INTO types (name_en) VALUES (:name_en)');
+            $statement = $this->pdo->prepare('INSERT INTO types (name_en, name_fr) VALUES (:name_en, :name_fr)');
             $statement->bindParam(':name_en', $name_en);
+            $statement->bindParam(':name_fr', $name_fr);
             $statement->execute();
             $id = $this->pdo->lastInsertId();
     
-            return new Type($id, $name_en);
+            return new Type($id, $name_en, $name_fr);
         } catch (PDOException $e) {
             throw new Exception('Database error: ' . $e->getMessage());
+        }
+    }
+
+    // Get a type by its ID
+    public function getTypeById($id): ?Type {
+        try {
+            if (!is_integer($id) || $id < 1) {
+                throw new Exception('Type ID must be a positive integer.');
+            }
+            $query = "SELECT * FROM types WHERE id = :id";
+            $statement = $this->pdo->prepare($query);
+            $statement->bindParam(':id', $id);
+            $statement->execute();
+
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+            if ($result == false) {
+                return null;
+            }
+
+            return new Type($result['id'], $result['name_en'], $result['name_fr']);
+            
+        } catch (PDOException $e) {
+            throw new Exception('Database error: ' . $e->getMessage());
+        } catch (Exception $e) {
+            throw $e;
         }
     }
 }
---
+++ Tests/TypeRepositoryTest.php
@@ -0,0 +1,79 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestDox;
+require_once(__DIR__ . '/../Repositories/TypeRepository.php');
+require_once(__DIR__ . '/../Fixtures/Fixtures.php');
+
+final class TypeRepositoryTest extends TestCase {
+
+    private $pdo;
+    private $type_repository;
+    private $fixtures;
+
+    protected function setUp(): void {
+        $this->pdo = new PDO('mysql:host=localhost;dbname=test_pogoapiphp', 'root', '');
+        $database = $this->createMock(Database::class);
+        $database->method('getPdo')->willReturn($this->pdo);
+
+        $this->pdo->exec("DROP TABLE IF EXISTS types");
+        $this->pdo->exec(
+            'CREATE TABLE types (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name_en VARCHAR(255) NOT NULL UNIQUE,
+                name_fr VARCHAR(255) UNIQUE,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )'
+        );
+
+        $this->type_repository = new TypeRepository($database);
+
+        $this->fixtures = new Fixtures();
+        foreach($this->fixtures->typesFixtures() as $fixture) {
+            $this->pdo->exec("INSERT INTO types (name_en, name_fr) VALUES ('{$fixture['name_en']}', '{$fixture['name_fr']}')");
+        }
+
+    }
+
+    protected function tearDown(): void
+    {
+        $query = "DROP TABLE IF EXISTS types";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute();
+        $this->pdo = null;
+        $this->type_repository = null;
+        $this->fixtures = null;
+    }
+
+    #[TestDox('addType() saves a type in database.')]
+    public function testAddTypeValidData(): void {
+        $type = $this->type_repository->addType('Electric', 'Électrik');
+
+        $statement = $this->pdo->prepare("SELECT * FROM types WHERE name_en = :name_en");
+        $statement->bindParam(':name_en', $type->getNameEn());
+        $statement->execute();
+        $results = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $this->assertEquals($type->getNameEn(), $results['name_en']);
+        $this->assertEquals($type->getNameFr(), $results['name_fr']);
+        $this->assertInstanceOf(Type::class, $type);
+    }
+
+    #[TestDox('getTypeById() throws an error when provided an invalid ID.')]
+    public function testGetTypeByIdInvalidId(): void {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Type ID must be a positive integer.');
+        $type = $this->type_repository->getTypeById('a');
+    }
+
+    #[TestDox('getTypeById() retrieves a Type when provided a valid and existing ID.')]
+    public function testGetTypByIdValidId(): void {
+        $type_searched = $this->fixtures->typesFixtures()[0];
+        $type = $this->type_repository->getTypeById(1);
+
+        $this->assertInstanceOf(Type::class, $type);
+        $this->assertEquals($type->getNameEn(), $type_searched['name_en']);
+    }
+
+}
